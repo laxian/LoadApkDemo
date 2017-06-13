5f394bd7c562c6b7c6adcb2dbf26333288d51db0
zhouweixian <laxian2010@qq.com>
2017-06-13 11:15:31 +0800

    添加对apk中类的加载

--- app/src/main/java/com/zhouweixian/host/MainActivity.java
+++ app/src/main/java/com/zhouweixian/host/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,13 +18,13 @@ import java.lang.reflect.Method;
 
 import dalvik.system.DexClassLoader;
 
-public class MainActivity extends Activity {
 
+@SuppressWarnings("all")
+public class MainActivity extends Activity {
 
     private String apkDir = Environment.getExternalStorageDirectory().getPath() + File.separator;
     private static final String apkName = "guestapk-debug.apk";
     private static final String pkgName = "com.zhouweixian.guest";
-    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +32,43 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ImageView imageView = (ImageView) findViewById(R.id.icon);
         TextView textView = (TextView) findViewById(R.id.text);
+        TextView editText = (TextView) findViewById(R.id.et_output);
 
-        resources = getPluginResources(apkName);
+        String apkFullPath = apkDir + File.separator + apkName;
+        Resources resources = getPluginResources(apkFullPath);
+        DexClassLoader dexClassLoader = getDexClassLoader(apkFullPath);
+
         try {
-            int imgId = getResIdFromApk(apkDir, apkName, pkgName, "drawable", "guest_img");
-            Drawable drawable = resources.getDrawable(imgId);
-            imageView.setImageDrawable(drawable);
+            // 从apk 中读取drawable 资源
+            int imgId = getResIdFromApk(dexClassLoader, pkgName, "drawable", "guest_img");
+            Drawable drawable;
+            if (resources != null) {
+                drawable = resources.getDrawable(imgId);
+                imageView.setImageDrawable(drawable);
+            }
 
-            int txtId = getResIdFromApk(apkDir, apkName, pkgName, "string", "guest_text");
-            String txt = resources.getString(txtId);
-            textView.setText(txt);
+            // 从apk中读取string 资源
+            int txtId = getResIdFromApk(dexClassLoader, pkgName, "string", "guest_text");
+            String txt;
+            if (resources != null) {
+                txt = resources.getString(txtId);
+                textView.setText(txt);
+            }
+
+            // 从apk中加载类
+            Class<?> demoClass = getClassFromApk(dexClassLoader, pkgName, "Demo");
+
+            // 反射调用对象方法
+            Method func1 = demoClass.getMethod("func1");
+            String func1Msg = (String) func1.invoke(demoClass.newInstance());
+            editText.setText(func1Msg);
+
+            // 反射调用类方法
+            Method func2 = demoClass.getMethod("func2");
+            String func2Msg = (String) func2.invoke(demoClass);
+            editText.setText(String.format("func1 -> %s \nfunc2 -> %s", editText.getText(), func2Msg));
+            Log.d("abc", demoClass.toString());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -49,30 +78,12 @@ public class MainActivity extends Activity {
     /**
      * 加载apk获得内部资源
      *
-     * @param apkDir         存放apk的目录
-     * @param apkName        apk文件名
      * @param apkPackageName apk文件的包名
      * @param resType        (anim, attr,bool,color,dimen,drawable,id,integer,layout,mipmap,string,style,styleable)
      * @param resName        带查找的资源名
-     * @throws ClassNotFoundException
-     * @throws NoSuchFieldException
-     * @throws IllegalAccessException
      */
-    private int getResIdFromApk(String apkDir, String apkName, String apkPackageName, String resType, String resName)
+    public int getResIdFromApk(DexClassLoader dexClassLoader, String apkPackageName, String resType, String resName)
             throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
-
-        //在应用安装目录下创建一个名为app_dex文件夹目录,如果已经存在则不创建
-        File optimizedDirectoryFile = getDir("dex", Context.MODE_PRIVATE);
-        //参数：
-        // 1、包含dex的apk文件或jar文件的路径;
-        // 2、apk、jar解压缩生成dex存储的目录;
-        // 3、本地library库目录，一般为null;
-        // 4、父ClassLoader;
-        DexClassLoader dexClassLoader = new DexClassLoader(
-                apkDir + File.separator + apkName,
-                optimizedDirectoryFile.getPath(),
-                null,
-                ClassLoader.getSystemClassLoader());
 
         //通过使用apk自己的类加载器，反射出R类中相应的内部类进而获取我们需要的资源id
         Class<?> clazz = dexClassLoader.loadClass(apkPackageName + ".R$" + resType);
@@ -82,20 +93,49 @@ public class MainActivity extends Activity {
         return field.getInt(R.id.class);
     }
 
+    @NonNull
+    private DexClassLoader getDexClassLoader(String apkFullPath) {
+        //在应用安装目录下创建一个名为app_dex文件夹目录,如果已经存在则不创建
+        File optimizedDirectoryFile = getDir("dex", Context.MODE_PRIVATE);
+        //参数：
+        // 1、包含dex的apk文件或jar文件的路径;
+        // 2、apk、jar解压缩生成dex存储的目录;
+        // 3、本地library库目录，一般为null;
+        // 4、父ClassLoader;
+        return new DexClassLoader(
+                apkFullPath,
+                optimizedDirectoryFile.getPath(),
+                null,
+                ClassLoader.getSystemClassLoader());
+    }
+
+
     /**
-     * @param apkName
+     * 加载apk，并返回{className}指定的类
+     *
+     * @param apkPackageName apk文件的包名
+     * @param className      类名
+     * @return className对应的类
+     */
+    public Class<?> getClassFromApk(DexClassLoader dexClassLoader, String apkPackageName, String className) throws ClassNotFoundException {
+
+        //通过使用apk自己的类加载器，反射出R类中相应的内部类进而获取我们需要的资源id
+
+        return dexClassLoader.loadClass(apkPackageName + "." + className);
+    }
+
+    /**
      * @return 得到对应插件的Resource对象
      */
-    private Resources getPluginResources(String apkName) {
+    private Resources getPluginResources(String apkFullPath) {
         try {
             AssetManager assetManager = AssetManager.class.newInstance();
             //反射调用方法addAssetPath(String path)
             Method addAssetPath = assetManager.getClass().getMethod("addAssetPath", String.class);
             //将未安装的Apk文件的添加进AssetManager中，第二个参数为apk文件的路径带apk名
-            addAssetPath.invoke(assetManager, apkDir + File.separator + apkName);
+            addAssetPath.invoke(assetManager, apkFullPath);
             Resources superRes = this.getResources();
-            return new Resources(assetManager, superRes.getDisplayMetrics(),
-                    superRes.getConfiguration());
+            return new Resources(assetManager, superRes.getDisplayMetrics(), superRes.getConfiguration());
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ guestapk/src/main/java/com/zhouweixian/guest/Demo.java
@@ -0,0 +1,15 @@
+package com.zhouweixian.guest;
+
+/**
+ * Created by leochou on 2017/6/13 10:32
+ */
+
+public class Demo {
+    public String func1() {
+        return "words from Demo func1()";
+    }
+
+    public static String func2() {
+        return "words from func2";
+    }
+}
